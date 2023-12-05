e09169edd7fc083539873e34814ae6ee0d7df717
UzziKyle <uzzielkyleynciong0824@gmail.com>
2023-12-06 05:32:33 +0800

    restructure folder

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from typing import Optional, Tuple, Union
-from components.server_interface.countdown_timer import CountdownTimer
-from components.server_interface.window import Window
+from gui_components.server_interface.countdown_timer import CountdownTimer
+from gui_components.server_interface.window import Window
 from customtkinter import *
 
         
@@ -16,11 +16,11 @@ class ServerInterface(CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(list(range(1)), weight=1)
         
-        self.window = Window(self)
-        self.window.grid(row=0, column=0, padx=8, pady=8, sticky="nsew")
+        self.window = Window(self, corner_radius=8)
+        self.window.grid(row=0, column=0, padx=8, pady=(8, 0), sticky="nsew")
         
         self.countdown_timer = CountdownTimer(self)
-        self.countdown_timer.grid(row=1, column=0, padx=8, pady=(0, 8), sticky="nsew")
+        self.countdown_timer.grid(row=1, column=0, padx=8, sticky="nsew")
         
         
 if __name__ == '__main__':
