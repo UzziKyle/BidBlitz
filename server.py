9400bb72431c434dc28cd69c3732953db852b182
UzziKyle <uzzielkyleynciong0824@gmail.com>
2023-12-09 20:32:03 +0800

    add banyan to server.py

--- server.py
+++ server.py
@@ -1,30 +1,46 @@
-from typing import Optional, Tuple, Union
-from gui_components.server_interface.countdown_timer import CountdownTimer
-from gui_components.server_interface.window import Window
-from customtkinter import *
+from python_banyan.banyan_base import BanyanBase
+from gui.server import ServerInterface
 
+
+class Server(BanyanBase):
+    def __init__(self, ):
+        super(Server, self).__init__(process_name="Server")
         
-class ServerInterface(CTk):
-    def __init__(self, fg_color: str | Tuple[str, str] | None = None, **kwargs):
-        super().__init__(fg_color, **kwargs)
+        self.set_subscriber_topic('for_sale')
         
-        self.title('SERVER')
-        self.geometry('375x500')
-        self.minsize(width=375, height=250)
-        set_appearance_mode('System')
+        self.for_sale = {}
+        self.items = []
+        self.bidders = {}
+        '''
+            {
+                "item": {
+                    "bidders": {
+                        "name": username,
+                        "bid": amount,
+                    },
+                },
+            }
+        '''
         
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(list(range(1)), weight=1)
+        self.interface = ServerInterface()
+        self.interface.bind("<Destroy>", self.on_destroy)
+        self.interface.mainloop()
         
-        self.window = Window(self, corner_radius=8)
-        self.window.grid(row=0, column=0, padx=8, pady=(8, 0), sticky="nsew")
+        self.receive_loop()
+                
+    def incoming_message_processing(self, topic, payload):
+        print('New Item: ', payload['item'])
+        self.items.append(payload['item'])
+        self.publish_payload(payload={'items': self.items}, topic='selling')
         
-        # TODO center position this
-        self.countdown_timer = CountdownTimer(self)
-        self.countdown_timer.grid(row=1, column=0, padx=8, sticky="nsew")
+    def on_destroy(self, event):
+        if event.widget != self.interface:
+            return
+        
+        self.clean_up()
+        exit()
         
         
 if __name__ == '__main__':
-    server_interface = ServerInterface()
-    server_interface.mainloop()
+    Server()
     
